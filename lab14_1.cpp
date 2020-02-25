ae4f88d6a0a693da41a4192c39a963744d1a5b69
nayrunner <59276659+nayrunner@users.noreply.github.com>
2020-02-25 15:15:15 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,67 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
-	//Write your code here.
+	int a =5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int* *z = &x;
 	
-	
-	
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+    cout<<"c = "<<c<<endl;
+	cout<<"x = "<<x<<endl;
+	cout<<"y = "<<y<<endl;
+	cout<<"z = "<<z<<endl<<"\n";
+
+	cout<<"&a = "<<&a<<endl;
+	cout<<"&b = "<<&b<<endl;
+    cout<<"&c = "<<&c<<endl;
+	cout<<"&x = "<<&x<<endl;
+	cout<<"&y = "<<&y<<endl;
+	cout<<"&z = "<<&z<<endl<<"\n";
+
+    c = 'F';
+
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+    cout<<"c = "<<c<<endl;
+	cout<<"x = "<<x<<endl;
+	cout<<"y = "<<y<<endl;
+	cout<<"z = "<<z<<endl<<"\n";
+
+	*y = 'W';
+
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+    cout<<"c = "<<c<<endl;
+	cout<<"x = "<<x<<endl;
+	cout<<"y = "<<y<<endl;
+	cout<<"z = "<<z<<endl<<"\n";
+
+	*x = 6;
+
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+    cout<<"c = "<<c<<endl;
+	cout<<"x = "<<x<<endl;
+	cout<<"y = "<<y<<endl;
+	cout<<"z = "<<z<<endl<<"\n";
+
+	**z = 7;
+
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+    cout<<"c = "<<c<<endl;
+	cout<<"x = "<<x<<endl;
+	cout<<"y = "<<y<<endl;
+	cout<<"z = "<<z<<endl<<"\n";
+
+
+
+
 	return 0;
 }
